f9520096d959dd7eebc952c688fd70a24094f5e3
SashaVoloshyn <sasha.voloshyn359@gmail.com>
2022-01-13 18:34:29 +0200

    hw1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 import Comments from './components/Comments';
 import Users from './components/Users';
 import Posts from './components/Posts';
 
-import {useState} from 'react';
-import {useEffect} from 'react';
 
 
 function App() {
 
     return (
-        <div className={'wrap'}>
+        <div className='wrap'>
             <div className="usersPosts">
                 <div className="usersPosts__elements">
                     <Users/>
